8e70fe422b365290e7cf7c95e00276efa6c18092
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-02-02 16:27:31 -0600

    use standard hydration setup

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,11 +1,23 @@
 import "@master/css"
 import { RemixBrowser } from "@remix-run/react"
-import { StrictMode } from "react"
+import { startTransition, StrictMode } from "react"
 import { hydrateRoot } from "react-dom/client"
 
-hydrateRoot(
-  document,
-  <StrictMode>
-    <RemixBrowser />
-  </StrictMode>,
-)
+function hydrate() {
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <RemixBrowser />
+      </StrictMode>,
+    )
+  })
+}
+
+if (typeof requestIdleCallback === "function") {
+  requestIdleCallback(hydrate)
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  setTimeout(hydrate, 1)
+}
